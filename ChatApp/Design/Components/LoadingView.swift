3ec4a1749a0a63108501cbdf729f2a3f1159fc49
AdiIncc <adrian.inculet@gmail.com>
2025-10-07 14:58:33 +0300

    Added loading view.

---
+++ ChatApp/Design/Components/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  ChatApp
+//
+//  Created by Adrian Inculet on 07.10.2025.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
